7750c48a044b38d367bc188a6f5c99f5cd66c06f
Justin Bell <justinbell0114@gmail.com>
2026-08-16 01:32:16 -0400

    got rid of build

--- tb/golden/build/harness.d
+++
@@ -1,6 +0,0 @@
-harness.o: /home/juiceboy/Small_TPU_Revamped/tb/golden/harness.cpp \
- /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_config.h \
- /usr/share/verilator/include/verilated_types.h \
- /usr/share/verilator/include/verilated_funcs.h Vtpu_top.h
